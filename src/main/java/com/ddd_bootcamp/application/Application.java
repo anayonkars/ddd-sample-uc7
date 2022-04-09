c73741df1c62a1a485dc3f6b61955de98a00c11a
Sandeep Jagtap <sandeep.jagtap@gmail.com>
2022-04-09 16:11:16 +0530

    Cleanup

--- src/main/java/com/ddd_bootcamp/application/Application.java
+++ src/main/java/com/ddd_bootcamp/application/Application.java
@@ -13,18 +13,21 @@ public class Application {
     public static void main(String[] args) {
         Cart cart = new Cart();
 
-        Product headphone = new Product("Sony Wireless headphone", new Price(BigDecimal.valueOf(10), Currency.getInstance("USD")));
-        Item headphoneItem = new Item(headphone,1);
+        Product headphone = new Product("Sony Wireless headphone",
+                new Price(BigDecimal.valueOf(10), Currency.getInstance("USD")));
+        Item headphoneItem = new Item(headphone, 1);
         cart.add(headphoneItem);
 
-        Product applePencil = new Product("Apple Pencil", new Price(BigDecimal.valueOf(100), Currency.getInstance("USD")));
+        Product applePencil = new Product("Apple Pencil",
+                new Price(BigDecimal.valueOf(100), Currency.getInstance("USD")));
         Item applePencilItem = new Item(applePencil, 2);
         cart.add(applePencilItem);
 
         List<Item> items = cart.getItems();
         System.out.println("items = " + items);
 
-        Product applePencil1 = new Product("Apple Pencil", new Price(BigDecimal.valueOf(10), Currency.getInstance("USD")));
+        Product applePencil1 = new Product("Apple Pencil",
+                new Price(BigDecimal.valueOf(10), Currency.getInstance("USD")));
         Item applePencilItem1 = new Item(applePencil1, 2);
 
         cart.remove(applePencilItem1);
@@ -48,16 +51,18 @@ public class Application {
         Cart cart1 = new Cart();
         Cart cart2 = new Cart();
 
-        Product headphone1 = new Product("Sony Wireless headphone", new Price(BigDecimal.valueOf(10), Currency.getInstance("USD")));
-        Item headphoneItem1 = new Item(headphone1,1);
+        Product headphone1 = new Product("Sony Wireless headphone",
+                new Price(BigDecimal.valueOf(10), Currency.getInstance("USD")));
+        Item headphoneItem1 = new Item(headphone1, 1);
 
-        Product headphone2 = new Product("Sony Wireless headphone", new Price(BigDecimal.valueOf(10), Currency.getInstance("USD")));
-        Item headphoneItem2= new Item(headphone2,1);
+        Product headphone2 = new Product("Sony Wireless headphone",
+                new Price(BigDecimal.valueOf(10), Currency.getInstance("USD")));
+        Item headphoneItem2 = new Item(headphone2, 1);
 
         cart1.add(headphoneItem1);
         cart2.add(headphoneItem2);
 
-        System.out.println(cart1.equals(cart2)? "Carts are same" : "Carts are different");
-        System.out.println(cart1.hasSameIdentityAs(cart2)? "Carts are same"  : "Carts are different");
+        System.out.println(cart1.equals(cart2) ? "Carts are same" : "Carts are different");
+        System.out.println(cart1.hasSameIdentityAs(cart2) ? "Carts are same" : "Carts are different");
     }
 }
